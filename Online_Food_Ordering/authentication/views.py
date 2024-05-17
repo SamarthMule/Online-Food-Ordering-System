80b9b13e02a4fed17c4f35c81f44129e1d322506
Tulika <mstulika0512@gmail.com>
2024-05-17 16:04:13 +0530

    uploading the changes

--- Online_Food_Ordering/authentication/views.py
+++
@@ -1,8 +0,0 @@
-
-from django.shortcuts import render
-
-def login_view(request):
-    return render(request, 'authentication/login.html')
-
-def registration_view(request):
-    return render(request, 'authentication/registration.html')
